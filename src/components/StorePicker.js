f7a791090e989d03f3fa46d9a51bfb1f57083b07
in-in <dmin@tuta.io>
2019-02-24 07:47:16 +0300

    feat: init commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class StorePicker extends Component {
+  render() {
+    return (
+      <div>
+        <p>I'm a StorePicker</p>
+      </div>
+    );
+  }
+}
