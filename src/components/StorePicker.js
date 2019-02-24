9e8f4618f5a04de1620998526bfac98efeca452e
in-in <dmin@tuta.io>
2019-02-24 11:32:34 +0300

    feat(styled-components): add styled-components 💅

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h2`
+  font-style: italic;
+`;
 
 export default class StorePicker extends Component {
   render() {
     return (
-      <form className="store__selector">
-        <h2 className="store__title">Please Enter A Store</h2>
-        <input type="text" className="store__input" />
+      <form className="store__picker">
+        <Title>Please Enter A Store</Title>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          className="store__input"
+        />
         <button type="submit" className="store__submit">
           Visit Store →
         </button>
