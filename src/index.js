488664b416d57d23cbfd5f3f03693127b06f73a9
in-in <dmin@tuta.io>
2019-02-25 13:31:59 +0300

    feat: add components: App, Header, Inventory, NotFound, Order, Router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
+import Router from './components/Router';
 
-render(<StorePicker />, document.getElementById('root'));
+render(<Router />, document.getElementById('root'));
