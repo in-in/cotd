fa8212f35196980bfd70887dfde2ffa6c5b441fd
in-in <dmin@tuta.io>
2019-03-27 01:48:16 +0300

    refactor(NotFound): stylize NotFound component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components/macro";
+import Title from "./Title";
+import Wrapper from "./Wrapper";
+
+const Background = styled.div`
+  max-width: 500px;
+  padding: 1em;
+  text-align: center;
+  background-color: ${props => props.theme.primary.whiteSmoke};
+`;
+
+const Code = styled.p`
+  color: ${props => props.theme.primary.redVivid};
+  text-overflow: ellipsis;
+  overflow: hidden;
+  font-family: monospace;
+  font-size: 1.3em;
+`;
 
 const NotFound = ({ location }) => (
-  <div>
-    <h2>
-      Not found
-      <code>{location.pathname}</code>
-    </h2>
-  </div>
+  <Wrapper>
+    <Background>
+      <Title>Not found </Title>
+      <Code>{location.pathname}</Code>
+    </Background>
+  </Wrapper>
 );
 
 NotFound.propTypes = {
-  location: PropTypes.string.isRequired
+  location: PropTypes.shape({
+    pathname: PropTypes.string
+  }).isRequired
 };
 
 export default NotFound;
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components/macro";
 
 const StyledWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
